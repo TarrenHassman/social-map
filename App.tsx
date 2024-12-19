7dfd38b5de14c0008faa4ca84993d8b4c8194456
cyberfly <info@cyberfly.io>
2024-12-19 21:30:35 +0530

    first commit

--- App.tsx
+++ App.tsx
@@ -1,118 +1,64 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import './globals.js';
+import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { createLibp2p } from 'libp2p';
+import debug from 'debug'
+import { config } from './config/libp2pconfig';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+debug.enable('libp2p:*,*:trace')
+export default function App() {
+  const [libp2p, setLibp2p] = useState(null);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  useEffect(() => {
+    async function getLibp2p() {
+      try {
+        const node = await createLibp2p(config);
+        console.log(node.peerId.toString())
+        if (node) {
+          console.log('Libp2p node created:', node);
+          setLibp2p(node);
+        } else {
+          console.error('Libp2p node creation returned null');
+        }
+      } catch (error) {
+        console.error('Error creating Libp2p node:', error);
+      }
+    }
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+    getLibp2p();
+  }, []);
+
+  useEffect(()=>{
+if(libp2p){
+  libp2p.addEventListener("peer:discovery", (x) => {
+    console.log(
+      "Discovered: ",
+      x.detail.id.toString(),
+      x.detail.multiaddrs.map((a) => a.toString()),
+    );
+  });
+  libp2p.addEventListener("peer:connect", () => {
+    console.log("Peers: ", libp2p.getPeers());
+  });
+}
+  },[libp2p])
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <Text>js-libp2p running on React Native</Text>
+      <Text>Our PeerId is {libp2p?.peerId.toString()}</Text>
+
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    fontSize: 20,
+    fontStyle: 'italic',
+    alignItems: 'center',
   },
 });
-
-export default App;

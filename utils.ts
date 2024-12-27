2435cf16e27454645a1d9abd8de0b8a2df2ca08a
cyberfly <info@cyberfly.io>
2024-12-27 11:38:23 +0530

    level storage support

--- utils.ts
+++
@@ -1,9 +0,0 @@
-export const toSortJson = (data:any)=>{
-    const sortedJsondata = Object.keys(data)
-    .sort() // Sort the keys
-    .reduce((obj, key) => {
-        obj[key] = data[key]; // Build a new sorted object
-        return obj;
-    }, {});
-    return sortedJsondata
-  }
